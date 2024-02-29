7bf605d0c51cc01c49ea845c0100664927958534
Karthik Charan Raghunathan <karthikchrn@eis-karthik-adapter1.lan.ini.uzh.ch>
2024-02-29 17:30:13 +0100

    added rlif and utils

--- losses.py
+++ losses.py
@@ -1,8 +1,12 @@
-import jax.numpy as np
+import jax.numpy as jnp
 from jax.nn import log_softmax
 
 
 def fr_decoder(spikes):
     """ Calculates log-softmax based on spike count logits. """
-    Yhat = log_softmax(np.sum(spikes, 1))
+    Yhat = log_softmax(jnp.sum(spikes, 1))
     return Yhat
+
+@jit
+def l2_loss(x, alpha):
+    return alpha * (x ** 2).mean()
